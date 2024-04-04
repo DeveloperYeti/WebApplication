9612b1953166353d23a4af3f1ae6be2bac4aef42
DeveloperYeti <hoseok9823@gmail.com>
2024-04-04 11:51:58 +0900

    0404

--- src/main/java/org/example/webapplication0404/controller.java
+++ src/main/java/org/example/webapplication0404/controller.java
@@ -1,8 +1,15 @@
 package org.example.webapplication0404;
 
+import org.example.webapplication0404.dto.SampleDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/sample")
@@ -11,4 +18,20 @@ public class controller {
     public void ex(){
 
     }
+    @GetMapping({"/ex2","/ex2_1"})
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                .sno(i)
+                .first("first.." + i)
+                .last("Last.." + i)
+                .regTime(LocalDateTime.now())
+                .build();
+            return dto;
+        }).collect(Collectors.toList());
+        model.addAttribute("list", list);
+
+    }
+
 }
+
---
+++ src/main/java/org/example/webapplication0404/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package org.example.webapplication0404.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
