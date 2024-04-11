db07a60fbab60ce38f7b76864e2970db49c0774f
DeveloperYeti <hoseok9823@gmail.com>
2024-04-11 10:15:50 +0900

    thymeleafRedirect

--- src/main/java/org/example/webapplication0404/SampleController.java
+++ src/main/java/org/example/webapplication0404/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/sample")
-public class controller {
+public class SampleController {
     @GetMapping("/ex1")
     public void ex(){
 
@@ -33,5 +34,23 @@ public class controller {
 
     }
 
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes){// 매개변수의 타입이 나올 수 있음 시험으로
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("first...100")
+                .last("last")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");// addFlashAttribute 의 속성 값을 사용 할 수 있는가
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";//redirect mapping 주소 명시할 수 있는지
+    }
+    @GetMapping("/ex3")
+    public void ex3(){
+        
+    }
 }
 
