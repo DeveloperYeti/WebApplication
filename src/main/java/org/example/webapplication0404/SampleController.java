dac61247c5fbed998421bab9b0a05e48947731ce
DeveloperYeti <hoseok9823@gmail.com>
2024-06-15 17:28:30 +0900

    졸프 사용 (서버)

--- src/main/java/org/example/webapplication0404/SampleController.java
+++ src/main/java/org/example/webapplication0404/SampleController.java
@@ -52,7 +52,7 @@ public class SampleController {
     public void ex3(){
         
     }
-    @GetMapping({"/exLayout1","/exLayout2","/exTemplate"})
+    @GetMapping({"/exLayout1","/exLayout2","/exTemplate","/exSidebar"})
     public void exLaytout(){
 
     }
